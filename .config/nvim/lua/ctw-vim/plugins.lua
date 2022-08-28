d97c9c60d9f81dfc1cb52fee0f2be848de5ba0f0
Alexander Saprykin <cutwatercore@gmail.com>
2022-08-28 22:38:13 +0200

    neovim: Setup `gruvbox` theme

--- .config/nvim/lua/ctw-vim/plugins.lua
+++ .config/nvim/lua/ctw-vim/plugins.lua
@@ -2,11 +2,22 @@ local M = {}
 
 local plugins = {
   { "wbthomason/packer.nvim" },
+  -- Theme
+  {
+    "gruvbox-community/gruvbox",
+    config = function()
+      vim.cmd 'colorscheme gruvbox'
+    end
+  },
 }
 
 function M.setup()
   packer = require("packer")
-  packer.startup(plugins)
+  packer.startup(function(use)
+    for _, plugin in pairs(plugins) do
+      use(plugin)
+    end
+  end)
 end
 
 return M
