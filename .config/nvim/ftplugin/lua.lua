db079d1cb0e47a5cd00dd3b9a5ca712760e3a124
Alexander Saprykin <cutwatercore@gmail.com>
2023-05-30 16:34:13 +0200

    Reset neovim config

--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
-local bo = vim.bo
-
-bo.shiftwidth = 2
-bo.tabstop = 2
-bo.softtabstop = 2
--- .config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require('ctw-vim/plugins').setup()
-require('ctw-vim/options').setup()
-require('ctw-vim/mappings').setup()
