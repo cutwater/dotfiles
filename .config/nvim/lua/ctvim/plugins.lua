12caf9b99cf3a9eb793b2fc888867b0457d414a1
Alexander Saprykin <cutwatercore@gmail.com>
2023-11-26 10:25:05 +0100

    [nvim] Update telescope to 0.1.4

--- .config/nvim/lua/ctvim/plugins.lua
+++ .config/nvim/lua/ctvim/plugins.lua
@@ -16,7 +16,7 @@ local plugins = {
     },
     {
         'nvim-telescope/telescope.nvim', 
-        tag = '0.1.1',
+        tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     },
     {
