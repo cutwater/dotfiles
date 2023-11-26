a7a9273bc6e341e50bd1579c33589330faf09c52
Alexander Saprykin <cutwatercore@gmail.com>
2023-11-26 10:08:01 +0100

    [nvim] Configure treesitter
    * Disable default parsers
    * Disable auto install

--- .config/nvim/lua/ctvim/plugins/treesitter.lua
+++ .config/nvim/lua/ctvim/plugins/treesitter.lua
@@ -11,12 +11,14 @@ local opts = {
       "bash",
   },
 
+  ignore_install = { "all" },
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  auto_install = false,
 
   highlight = {
     enable = true,
