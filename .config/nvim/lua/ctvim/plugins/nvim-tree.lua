74aa7a492bfc4fd8ecbe7c6c509db37bd62310f6
Alexander Saprykin <cutwatercore@gmail.com>
2023-11-26 10:07:30 +0100

    [nvim] Add `?` hotkey to nvim-tree

--- .config/nvim/lua/ctvim/plugins/nvim-tree.lua
+++ .config/nvim/lua/ctvim/plugins/nvim-tree.lua
@@ -1,10 +1,29 @@
-local opts = {
-    view = {
-        width = 40,
-    },
-}
+local function on_attach(bufnr)
+    local api = require('nvim-tree.api')
+
+    local function opt(desc)
+        return {
+            desc = 'nvim-tree: ' .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+        }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set('n', '?', api.tree.toggle_help, opt('Help'))
+end
 
 local function setup()
+    local opts = {
+        view = {
+            width = 40,
+        },
+        on_attach = on_attach,
+    }
+
     -- Disable netrw
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
