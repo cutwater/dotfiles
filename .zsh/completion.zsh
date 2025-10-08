27b7256d4cab6002d9714d5cba710e1f49d85020
Alexander Saprykin <cutwatercore@gmail.com>
2025-10-08 16:16:49 +0200

    feath(zsh): enable rustup autocompletions on MacOS

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,5 +1,11 @@
 # vim: ft=zsh
 
+# rustup
+if [[ "$_zshrc_platform" == 'macos'
+      && -d /opt/homebrew/opt/rustup/share/zsh/site-functions ]]; then
+  fpath+=/opt/homebrew/opt/rustup/share/zsh/site-functions
+fi
+
 autoload -Uz compinit && compinit
 autoload -Uz bashcompinit && bashcompinit
 
