b85a1ab421abe52f9ba68d21c00ce31659002d58
Alexander Saprykin <cutwatercore@gmail.com>
2026-03-31 18:34:32 +0200

    zsh: remove commented out NVM configuration

--- .zsh/environ.zsh
+++ .zsh/environ.zsh
@@ -60,10 +60,3 @@ if command -v pyenv &>/dev/null; then
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
 fi
-
-# NVM
-# if [[ -d "$HOME/.nvm" ]]; then
-#     export NVM_DIR="$HOME/.nvm"
-#     [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
-#     [[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
-# fi
